084133b6fc2e0a338fa881774c71bfef7b19c1e4
Jackson Holliday <jaxholliday@gmail.com>
2023-03-20 18:04:49 -0400

    initial

--- js/site.js
+++ js/site.js
@@ -1,4 +1,19 @@
-//call Hello Word 
-function HelloWorld(){
-    alert("Hello World");
+//get our user Input
+function getValue(){
+    //get user string
+    document.getElementById("userString").value;
+
+    // check for palindrome
+    let returnObj = checkForPalindrome(userString);
+    // display the message to the screen 
+}
+
+//check if the string is a palindrome
+function checkForPalindrome(){
+
+}
+
+// display a message for the user
+function displayMessage(){
+
 }
